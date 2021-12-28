9ba10f0b48ca108a0806952b99f1f204f71ba4c9
Kazto TAKAHASHI <kazto@kazto.dev>
2021-12-28 12:49:53 +0900

    マイグレーションにコメント追加

--- db/migrate/20211228012541_create_users.rb
+++ db/migrate/20211228012541_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :slack_user_id
-      t.string :slack_user_name
-      t.bigint :total_send_points
-      t.bigint :total_receive_points
+      t.string :slack_user_id, comment: "SlackユーザID（プロフィール、メンバーIDをコピー、から取得できる）"
+      t.string :slack_user_name, comment: "Slackユーザ名（プロフィール、氏名。メンション時に表示されるもの）"
+      t.bigint :total_send_points, comment: "総感謝した数"
+      t.bigint :total_receive_points, comment: "総感謝された数"
 
       t.timestamps
     end
--- db/migrate/20211228015813_create_workspaces.rb
+++ db/migrate/20211228015813_create_workspaces.rb
@@ -1,7 +1,7 @@
 class CreateWorkspaces < ActiveRecord::Migration[7.0]
   def change
     create_table :workspaces do |t|
-      t.string :name
+      t.string :name, comment: "Slackワークスペース名"
 
       t.timestamps
     end
